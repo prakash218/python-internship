3e738e5f3a03179c1407a385e572ef47de79e717
Prakash Kumar <prakash218kumar@gmail.com>
2020-11-24 13:47:48 +0530

    Create Day 2 task.py

---
+++ Day 2 task.py
@@ -0,0 +1,32 @@
+#task A
+
+a = 10
+b = 10
+c = 10
+
+a //= 10
+b  *= 50
+c  += 60
+
+print(a,b,c)
+#output = 1 500 70
+
+#task B
+
+string = "EABLE"
+
+string = string[:2] + 'G' + string[3:]
+
+print(string)
+#output = EAGLE
+
+#task C
+
+a = 1000
+b = 100.0
+a = float(a)
+b = int(b)
+
+print(a,':',type(a),b,':',type(b))
+#output = 1000.0 : <class 'float'> 100 : <class 'int'>
+
