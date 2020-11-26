37ca4f42b9004dc032a7a469bc8c52ff4953b1c0
Prakash Kumar <prakash218kumar@gmail.com>
2020-11-26 17:14:14 +0530

    Create Day 4 task.py

---
+++ Day 4 task.py
@@ -0,0 +1,26 @@
+lst = [1,2,3,4,5,6,7]
+
+#Task 1
+lst.append(8)
+print(lst)
+
+lst.remove(1)
+print(lst)
+
+maximum = max(lst)
+minimum = min(lst)
+
+print(maximum,minimum)
+
+
+#Task 2
+
+my_tuple = ('prakash',19,'Male')
+
+print(my_tuple[::-1])
+
+#Task 3
+
+my_list = list(my_tuple)
+
+print(my_list)
