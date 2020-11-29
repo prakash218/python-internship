89607fbb316750c8fbcf3b6a5ba11a028d480ff0
Prakash Kumar <prakash218kumar@gmail.com>
2020-11-29 16:39:33 +0530

    creation of Day 7 task.py

---
+++ Day 7 task.py
@@ -0,0 +1,36 @@
+
+#Task 1
+
+import module
+
+module.my_list[0] += 10 #my_list = [1,2,3,4,5,6,7]
+
+print(module.my_list)
+
+#Task 2
+
+import pandas as pd
+
+print(pd.__path__)
+
+
+#Task 3
+
+from random import randint
+
+print(randint(0,100))
+
+
+#Task 4
+
+import sys
+
+print(sys.path)
+
+
+#Task 5
+
+#pip install pygame
+#pip uninstall pygame
+
+
